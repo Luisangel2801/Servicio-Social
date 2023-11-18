e348aafc30465ae51c72096f4add30ecd00e97e9
Luis Ángel Cruz Díaz <al2183038433@azc.uam.mx>
2023-11-18 13:00:06 -0600

    Fix LED pin assignments and add serial communication

--- 1 - Blink/src/main.cpp
+++ 1 - Blink/src/main.cpp
@@ -6,22 +6,25 @@
  *	para prueba de funcionamiento del IDE PlatformIO
  *
  *	ESP8266
- *	Pin 16 = Led integrado rojo
- *	Pin 2 = Led integrado azul
+ *	Led integrado rojo = Pin 16 (GPIO 2)
+ *	Led integrado azul = Pin 2 (GPIO 16)
  */
 
 #include <Arduino.h>
 
-const int led = 2; // GPIO 2
-// const int led = 16; 
+//const int led = 2;
+const int led = 16; 
 
-void setup() { 
-	pinMode(led, OUTPUT);    // inicializar GPIO 2 como salida 
+void setup() {
+	Serial.begin(9600);		// iniciar puerto serial
+	pinMode(led, OUTPUT);	// inicializar GPIO 2 como salida 
 } 
 
-void loop() {∫ 
-	digitalWrite(led, HIGH); // enciende el LED (HIGH es ALTO y es el nivel de voltaje)
-	delay(1000);             // espera 1000 milisegundos = un segundo
-	digitalWrite(led, LOW);  // apaga el LED (LOW es BAJO y es el nivel de voltaje)
-	delay(1000);             // espera 1000 miliasegundos = un segundo
+void loop() { 
+	digitalWrite(led, HIGH);	// apaga el LED (HIGH es ALTO y es el nivel de voltaje)
+	Serial.println("LED OFF");
+	delay(10000);				// espera 1000 milisegundos = un segundo
+	digitalWrite(led, LOW);		// enciende el LED (LOW es BAJO y es el nivel de voltaje)
+	Serial.println("LED ON");
+	delay(10000);				// espera 1000 miliasegundos = un segundo
 }
