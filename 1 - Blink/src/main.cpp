6339c94e8d181b5c0133f6c5a4275f0aeef18ac6
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2024-06-20 21:10:34 -0600

    Update configuration file and main file to work with ESP32 board

--- 1 - Blink/src/main.cpp
+++ 1 - Blink/src/main.cpp
@@ -2,29 +2,35 @@
  *	Autor: Luis Angel Cruz Diaz
  *	Fecha: 16/11/2023
  * 
- *	Este programa realiza el parpadep del LED integrado del ESP8266
+ *	Este programa realiza el parpadeo de un LED integrado en la placa ESP8266 y ESP32.
  *	para prueba de funcionamiento del IDE PlatformIO
  *
- *	ESP8266
- *	Led integrado rojo = Pin 16 (GPIO 2)
- *	Led integrado azul = Pin 2 (GPIO 16)
+ *  ESP8266
+ *  Led integrado rojo = Pin 16 (GPIO 2)
+ *  Led integrado azul = Pin 2 (GPIO 16)
+ *
+ *  ESP32
+ *  Led integrado = Pin 2 (D2)
  */
 
 #include <Arduino.h>
 
-//const int led = 2;
-const int led = 16; 
+#ifdef ESP8266_BOARD
+    int led = 16;
+#elif defined(ESP32_BOARD)
+    int led = 2;
+#endif
 
 void setup() {
 	Serial.begin(9600);		// iniciar puerto serial
-	pinMode(led, OUTPUT);	// inicializar GPIO 2 como salida 
+	pinMode(led, OUTPUT);	// inicializar pin de salida
 } 
 
 void loop() { 
-	digitalWrite(led, HIGH);	// apaga el LED (HIGH es ALTO y es el nivel de voltaje)
+	digitalWrite(led, HIGH);
 	Serial.println("LED OFF");
-	delay(10000);				// espera 1000 milisegundos = un segundo
-	digitalWrite(led, LOW);		// enciende el LED (LOW es BAJO y es el nivel de voltaje)
+	delay(1000);                // Tiempo de espera de 1 segundo
+	digitalWrite(led, LOW);
 	Serial.println("LED ON");
-	delay(10000);				// espera 1000 miliasegundos = un segundo
+	delay(1000);
 }
