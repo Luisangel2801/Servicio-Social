32eba34f38e0bb9da5af942dcd05347661f55399
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2024-12-18 03:32:03 -0600

    refactor(internet-connection): Integrate ESP32 and ESP8266 boards for WiFi connection via web page

--- 8_1 - Internet connection through a web page/src/main.cpp
+++ 8_1 - Internet connection through a web page/src/main.cpp
@@ -9,19 +9,33 @@
  *  el ESP32. Una vez que el ESP32 se haya conectado a la red WiFi seleccionada, se mostrará 
  *  la dirección IP asignada al dispositivo.
  * 
- *  Es necesario de agregar las siguientes librerías:
- *  - WiFi.h
- *  - WiFiManager.h by tzapu
- *  - DNSServer.h
- *  - WebServer.h
- *  - Ticker.h
+ * Es necesario de agregar las siguientes librerías:
+ * - WiFiManager.h by tzapu
+ * - DNSServer.h
+ * - Ticker.h
+ * - ESP8266WiFi.h (para ESP8266)
+ * - ESP8266WebServer.h (para ESP8266)
+ * - WiFi.h (para ESP32)
+ * - WebServer.h (para ESP32)
  */
 #include <Arduino.h>
-#include <WiFi.h>
 #include <WiFiManager.h>
 #include <DNSServer.h>
-#include <WebServer.h>
 #include <Ticker.h>
+
+#ifdef ESP8266_BOARD
+    #include <ESP8266WiFi.h>
+    #include <ESP8266WebServer.h>
+    int errorLedPin = 16;
+    bool ledEncendido = false;
+    bool ledApagado = true;
+#elif defined(ESP32_BOARD)
+    #include <WiFi.h>
+    #include <WebServer.h>
+    int errorLedPin = 21;
+    bool ledEncendido = true;
+    bool ledApagado = false;
+#endif
 
 Ticker ticker;          // Objeto para el manejo de interrupciones
 int statusLedPin = 2;   // Led integrado en el ESP32
@@ -35,9 +49,12 @@ void parpadeo() {
 void setup() {
     Serial.begin(115200);
     pinMode(statusLedPin, OUTPUT);
+    pinMode(errorLedPin, OUTPUT);
+    digitalWrite(errorLedPin, ledApagado);
+    digitalWrite(statusLedPin, ledApagado);
     ticker.attach(0.2, parpadeo);   // Inicialización del ticker
     WiFiManager wifiManager;        // Inicialización de la conexión a la red WiFi
-    //wifiManager.resetSettings();  // Descomenta esta línea si quieres resetear la configuración de WiFi
+    //wifiManager.resetSettings();    // Descomenta esta línea si quieres resetear la configuración de WiFi
     // Conexión a la red WiFi
     if (!wifiManager.autoConnect("ESP32_temporal", "12345678")) {
         Serial.println("Fallo en la conexión (timeout)");
@@ -46,10 +63,19 @@ void setup() {
     }
     // Detener el ticker
     ticker.detach();
-    digitalWrite(statusLedPin, LOW);
-    Serial.println("Conectado a WiFi");
-    Serial.print("Dirección IP: ");
-    Serial.println(WiFi.localIP());
+    if(WiFi.status() != WL_CONNECTED) {
+        Serial.println("Fallo en la conexión");
+        digitalWrite(errorLedPin, ledEncendido);
+        digitalWrite(statusLedPin, ledApagado);
+        delay(1000);
+        ESP.restart();
+    }else{
+        digitalWrite(errorLedPin, ledApagado);
+        digitalWrite(statusLedPin, ledEncendido);
+        Serial.println("Conectado a WiFi");
+        Serial.print("Dirección IP: ");
+        Serial.println(WiFi.localIP());
+    }
 }
 
 void loop() {
