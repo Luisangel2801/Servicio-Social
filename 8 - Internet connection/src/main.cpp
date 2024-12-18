6ca4875dacb65f8fda13843b42ee27d1eadb93a8
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2024-12-18 02:32:33 -0600

    refactor(Internet Connection): Integrate ESP32 and ESP8266 boards into the project

--- 8 - Internet connection/src/main.cpp
+++ 8 - Internet connection/src/main.cpp
@@ -3,12 +3,26 @@
  *  Autor: Luis Ángel Cruz Díaz
  *  Fecha: 18 de noviembre de 2023
  *
- *  Se requiere de la librería Wifi.h
- *  para el manejo de la conexión a red WiFi
+ * Se requiere de la librería Wifi.h para la conexión a la red WiFi en el ESP32
+ * Se requiere de la librería ESP8266WiFi.h para la conexión a la red WiFi en el ESP8266
+ * 
+ * En este código se muestra la secuencia de conexión a una red WiFi con un ESP32
+ * o un ESP8266. Se muestra el uso de los leds para indicar el estado de la conexión.
  */
 
 #include <Arduino.h>
-#include <WiFi.h>
+
+#ifdef ESP8266_BOARD
+    #include <ESP8266WiFi.h>
+    int errorLedPin = 16;
+    bool ledEncendido = false;
+    bool ledApagado = true;
+#elif defined(ESP32_BOARD)
+    #include <WiFi.h>
+    int errorLedPin = 21;
+    bool ledEncendido = true;
+    bool ledApagado = false;
+#endif
 
 // Variables de configuración de red
 const char *ssid = "SSID";          // Nombre de la red
@@ -18,7 +32,7 @@ const char *password = "PASSWORD";  // Contraseña de la red
 byte cont = 0;
 byte max_intentos = 50;
 int statusLedPin = 2;
-int errorLedPin = 21;
+bool ledState = false;
 
 void setup_wifi() {
     delay(10);
@@ -34,9 +48,11 @@ void setup_wifi() {
     // Secuencia que hace parpadear los leds durante el intento de conexión.
     while (WiFi.status() != WL_CONNECTED and cont < max_intentos) {
         cont++;
-        digitalWrite (statusLedPin, HIGH);
+        ledState = !ledState;
+        digitalWrite(statusLedPin, ledState);
         delay(500);
-        digitalWrite (statusLedPin, LOW);
+        ledState = !ledState;
+        digitalWrite (statusLedPin, ledState);
         Serial.print(".");  // Indicador de progreso
         delay (100); // Espera asimetrica para dar la sensación de parpadeo al led
     }
@@ -47,8 +63,8 @@ void setup_wifi() {
     // Mensajes de confirmación
     if(cont < max_intentos){
         Serial.println("\n*************************");
-        digitalWrite(statusLedPin, HIGH);
-        digitalWrite(errorLedPin, LOW);
+        digitalWrite(statusLedPin, ledEncendido);
+        digitalWrite(errorLedPin, ledApagado);
         Serial.print("Conectado a la red WiFi: ");
         Serial.println(WiFi.SSID());
         Serial.print("IP: ");
@@ -59,8 +75,8 @@ void setup_wifi() {
     }else{
         Serial.println("-------------------------------");
         Serial.println("Error de conexion");
-        digitalWrite (statusLedPin, LOW);
-        digitalWrite (errorLedPin, HIGH);
+        digitalWrite (statusLedPin, ledApagado);
+        digitalWrite (errorLedPin, ledEncendido);
         Serial.println("-------------------------------");
     }
 }
