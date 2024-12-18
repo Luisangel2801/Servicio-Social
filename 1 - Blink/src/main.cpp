be28cc3934ed566f5dd3b11bbf0fc8277d78d2d7
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2024-12-18 02:01:06 -0600

    fix(Blink): Update LED control logic for ESP8266 and ESP32 boards

--- 1 - Blink/src/main.cpp
+++ 1 - Blink/src/main.cpp
@@ -6,31 +6,34 @@
  *	para prueba de funcionamiento del IDE PlatformIO
  *
  *  ESP8266
- *  Led integrado rojo = Pin 16 (GPIO 2)
- *  Led integrado azul = Pin 2 (GPIO 16)
+ *  Led integrado rojo = Pin 16 (GPIO 2) - enciende con LOW
+ *  Led integrado azul = Pin 2 (GPIO 16) - enciende con LOW
  *
  *  ESP32
- *  Led integrado = Pin 2 (D2)
+ *  Led integrado azul = Pin 2 (D2) - enciende con HIGH
  */
 
 #include <Arduino.h>
 
 #ifdef ESP8266_BOARD
-    int led = 16;
+    int led = 2;
+    bool ledState = false;
 #elif defined(ESP32_BOARD)
     int led = 2;
+    bool ledState = true;
 #endif
 
 void setup() {
-	Serial.begin(9600);		// iniciar puerto serial
-	pinMode(led, OUTPUT);	// inicializar pin de salida
+    Serial.begin(9600);     // iniciar puerto serial
+    pinMode(led, OUTPUT);   // inicializar pin de salida
 } 
 
-void loop() { 
-	digitalWrite(led, HIGH);
-	Serial.println("LED OFF");
-	delay(1000);                // Tiempo de espera de 1 segundo
-	digitalWrite(led, LOW);
-	Serial.println("LED ON");
-	delay(1000);
+void loop() {
+    digitalWrite(led, ledState);
+    Serial.println("LED ON");
+    delay(5000);                // Tiempo de espera de 1 segundo
+    bool state = !ledState;
+    digitalWrite(led, state);
+    Serial.println("LED OFF");
+    delay(5000);
 }
